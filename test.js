5abe70ce191b67c27c35eb36d4744d74e631f152
Sarwar Ali <47899529+sarwar1227@users.noreply.github.com>
2020-05-13 15:47:56 +0530

    Multiple User Feature Added

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-var database=[{username:"Sarwar",password:"123"}];
+var database=[
+               {username:"Sarwar",password:"123"},
+               {username:"Virat",password:"456"},
+               {username:"Dhoni",password:"789"}
+             ];
 var newsfeed=[
                {username:"Virat",timeline:"Today,I made a century"},
                {username:"Dhoni",timeline:"I didn'y played well"},
@@ -6,15 +10,18 @@ var newsfeed=[
              ];
 entered_username=prompt("Enter Your Username ?");
 entered_password=prompt("Enter Your Password ?");
+function isUserValid(user,pass)
+{
+     for(var i=0;i<database.length;i++)
+     	if(user===database[i].username&&pass===database[i].password)
+     		return true;
+     return false;
+}
 function signIn(user,pass)
 {
-	if(user===database[0].username&&pass===database[0].password)
-	{
-		alert("Signed In...Check Console for the Newsfeed !!");
+	if(isUserValid(user,pass))	
 		console.log(newsfeed);
-    }
-	else{
+	else
 		alert("You Entered Wrong Username and Password !!");
-	}
 }
 signIn(entered_username,entered_password);
